cfe97947ed89b55b0d4b78d20a5a64e38d67a14b
Jesus Mejia <jesusmejded@gmail.com>
2018-05-17 19:14:52 -0500

    update test

--- test/test_all.py
+++ test/test_all.py
@@ -1,7 +1,7 @@
-from ecapy.optimizer import eca
+from ecapy import eca
 
 def test_eca():
-    f = lambda x, d: abs(sum(x[:d]))
-    x, fx = eca(f, 10, minimize=True)
+    f = lambda x: abs(sum(x))
+    x, fx = eca(f, 5, minimize=True)
 
     assert abs(fx) < 1e-8
